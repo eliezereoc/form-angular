e500b23c9016322e3119862ef33061cc0044fa1c
eliezereoc <eliezeroc@gmail.com>
2020-11-25 17:52:18 -0300

    adicionado modal e o formlario, precisa ajustar o modal.

--- formulario-angular/src/app/app.module.ts
+++ formulario-angular/src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 import { FormularioComponent } from './paginas/formulario/formulario.component';
 import { ListaCadComponent } from './compartilhado/lista-cad/lista-cad.component';
 import { FormCadComponent } from './compartilhado/form-cad/form-cad.component';
@@ -16,7 +28,12 @@ import { FormCadComponent } from './compartilhado/form-cad/form-cad.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    ReactiveFormsModule, NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
