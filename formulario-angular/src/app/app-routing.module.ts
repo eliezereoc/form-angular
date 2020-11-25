21333c97c36f256c5c6dc9cb160d84804f96217e
eliezereoc <eliezeroc@gmail.com>
2020-11-25 16:37:09 -0300

    adicionado form principal

--- formulario-angular/src/app/app-routing.module.ts
+++ formulario-angular/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormularioComponent } from './paginas/formulario/formulario.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FormularioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
